5ba366c2569a60efb09bf1d208bbf01ee25c3c3f
ikonilovelive <140684321+mykonshshhdhdhdh@users.noreply.github.com>
2023-08-10 23:20:43 +0800

    Add files via upload

---
+++ an.js
@@ -0,0 +1,104 @@
+"use strict";
+var y=1;
+function your(){
+var a = document.getElementById('a');a.style.transition = "1s"; a.style.height = "200px";
+var b = document.getElementById('b');b.style.transition = "1s"; b.style.height = "115px";
+var c = document.getElementById('c');c.style.transition = "1s"; c.style.height = "115px";
+var d = document.getElementById('d');d.style.transition = "1s"; d.style.height = "115px";
+var da = document.getElementById('da');da.style.transition = "1s"; da.style.opacity= "1";
+var db = document.getElementById('db');db.style.transition = "1s"; db.style.opacity= "0";
+var dc = document.getElementById('dc');dc.style.transition = "1s"; dc.style.opacity= "0";
+var dd = document.getElementById('dd');dd.style.transition = "1s"; dd.style.opacity = "0";
+
+}
+function b(){
+var a = document.getElementById('a');a.style.transition = "1s"; a.style.height = "115px";
+var b = document.getElementById('b');b.style.transition = "1s"; b.style.height = "200px";
+var c = document.getElementById('c');c.style.transition = "1s"; c.style.height = "115px";
+var d = document.getElementById('d');d.style.transition = "1s"; d.style.height = "115px";
+var da = document.getElementById('da');da.style.transition = "1s"; da.style.opacity= "0";
+var db = document.getElementById('db');db.style.transition = "1s"; db.style.opacity= "1";
+var dc = document.getElementById('dc');dc.style.transition = "1s"; dc.style.opacity= "0";
+var dd = document.getElementById('dd');dd.style.transition = "1s"; dd.style.opacity = "0";
+}
+function c(){
+var a = document.getElementById('a');a.style.transition = "1s"; a.style.height = "115px";
+var b = document.getElementById('b');b.style.transition = "1s"; b.style.height = "115px";
+var c = document.getElementById('c');c.style.transition = "1s"; c.style.height = "200px";
+var d = document.getElementById('d');d.style.transition = "1s"; d.style.height = "115px";
+var da = document.getElementById('da');da.style.transition = "1s"; da.style.opacity= "0";
+var db = document.getElementById('db');db.style.transition = "1s"; db.style.opacity= "0";
+var dc = document.getElementById('dc');dc.style.transition = "1s"; dc.style.opacity= "1";
+var dd = document.getElementById('dd');dd.style.transition = "1s"; dd.style.opacity = "0";
+}
+function d(){
+var a = document.getElementById('a');a.style.transition = "1s"; a.style.height = "115px";
+var b = document.getElementById('b');b.style.transition = "1s"; b.style.height = "115px";
+var c = document.getElementById('c');c.style.transition = "1s"; c.style.height = "115px";
+var d = document.getElementById('d');d.style.transition = "1s"; d.style.height = "200px";
+var da = document.getElementById('da');da.style.transition = "1s"; da.style.opacity= "0";
+var db = document.getElementById('db');db.style.transition = "1s"; db.style.opacity= "0";
+var dc = document.getElementById('dc');dc.style.transition = "1s"; dc.style.opacity= "0";
+var dd = document.getElementById('dd');dd.style.transition = "1s"; dd.style.opacity = "1";
+}
+function q(){
+ y=y+1;
+ if(y==6){
+ y=0;
+ }
+ if(y==1){
+ var z = document.getElementById('z');z.style.transition = "1s"; z.style.opacity= "1";
+var x = document.getElementById('x');x.style.transition = "1s"; x.style.opacity= "0";
+var v = document.getElementById('v');v.style.transition = "1s"; v.style.opacity= "0";
+var l = document.getElementById('l');l.style.transition = "1s"; l.style.opacity = "0";
+var n = document.getElementById('n');n.style.transition = "1s"; n.style.opacity = "0";
+var k = document.getElementById('k');k .style.transition = "1s"; k.style.opacity = "0";
+
+ }
+if(y==2){
+ var z = document.getElementById('z');z.style.transition = "1s"; z.style.opacity= "0";
+var x = document.getElementById('x');x.style.transition = "1s"; x.style.opacity= "1";
+var v = document.getElementById('v');v.style.transition = "1s"; v.style.opacity= "0";
+var l = document.getElementById('l');l.style.transition = "1s"; l.style.opacity = "0";
+var n = document.getElementById('n');n.style.transition = "1s"; n.style.opacity = "0";
+var k = document.getElementById('k');k .style.transition = "1s"; k.style.opacity = "0";
+ }
+ 
+ if(y==3){
+ var z = document.getElementById('z');z.style.transition = "1s"; z.style.opacity= "0";
+var x = document.getElementById('x');x.style.transition = "1s"; x.style.opacity= "0";
+var v = document.getElementById('v');v.style.transition = "1s"; v.style.opacity= "1";
+var l = document.getElementById('l');l.style.transition = "1s"; l.style.opacity = "0";
+var n = document.getElementById('n');n.style.transition = "1s"; n.style.opacity = "0";
+var k = document.getElementById('k');k .style.transition = "1s"; k.style.opacity = "0";
+
+ }
+ if(y==4){
+var z = document.getElementById('z');z.style.transition = "1s"; z.style.opacity= "0";
+var x = document.getElementById('x');x.style.transition = "1s"; x.style.opacity= "0";
+var v = document.getElementById('v');v.style.transition = "1s"; v.style.opacity= "0";
+var l = document.getElementById('l');l.style.transition = "1s"; l.style.opacity = "1";
+var n = document.getElementById('n');n.style.transition = "1s"; n.style.opacity = "0";
+var k = document.getElementById('k');k .style.transition = "1s"; k.style.opacity = "0";
+ }
+ if(y==5){
+var z = document.getElementById('z');z.style.transition = "1s"; z.style.opacity= "0";
+var x = document.getElementById('x');x.style.transition = "1s"; x.style.opacity= "0";
+var v = document.getElementById('v');v.style.transition = "1s"; v.style.opacity= "0";
+var l = document.getElementById('l');l.style.transition = "1s"; l.style.opacity = "0";
+var n = document.getElementById('n');n.style.transition = "1s"; n.style.opacity = "1";
+var k = document.getElementById('k');k .style.transition = "1s"; k.style.opacity = "0";
+ }
+ if(y==0){
+ var z = document.getElementById('z');z.style.transition = "1s"; z.style.opacity= "0";
+var x = document.getElementById('x');x.style.transition = "1s"; x.style.opacity= "0";
+var v = document.getElementById('v');v.style.transition = "1s"; v.style.opacity= "0";
+var l = document.getElementById('l');l.style.transition = "1s"; l.style.opacity = "0";
+var n = document.getElementById('n');n.style.transition = "1s"; n.style.opacity = "0";
+var k = document.getElementById('k');k .style.transition = "1s"; k.style.opacity = "1";
+ }
+ 
+ 
+
+ }
+
