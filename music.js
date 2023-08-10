b91bfaa440504ca1cc1fb72518e3ea1084b079b5
ikonilovelive <140684321+mykonshshhdhdhdh@users.noreply.github.com>
2023-08-10 17:02:51 +0800

    Add files via upload

---
+++ music.js
@@ -0,0 +1,34 @@
+
+bmusic=document.querySelector('.bmusic')
+amusic= document.querySelector('.amusic')
+
+function one(){
+	bmusic.pause()
+	amusic.src="平野绫、加藤英美里、福原香織、远藤綾 - もってけ！セーラーふく (拿去吧！水手服).mp3"
+	bmusic.load()
+    bmusic.play()
+}
+function two(){
+	bmusic.pause()
+	amusic.src="平野绫 - どんだけファンファーレ.mp3"
+	bmusic.load()
+    bmusic.play()
+}
+function three(){
+	bmusic.pause()
+	amusic.src="平野绫 - Dドライヴ ラヴ (D亡命驾驶 爱).mp3"
+	bmusic.load()
+    bmusic.play()
+}
+function four(){
+	bmusic.pause()
+	amusic.src="加藤英美里 - ケンカ予報の時間だよ (吵架预报的时间到了).mp3"
+	bmusic.load()
+    bmusic.play()
+}
+function five(){
+	bmusic.pause()
+	amusic.src="福原香織 - 寝・逃・げでリセット！ (躲进被窝 重新来过！).mp3"
+	bmusic.load()
+    bmusic.play()
+}
