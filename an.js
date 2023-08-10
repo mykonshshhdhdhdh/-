7e982bfc3ac192fba67e233786a3e82f9f7c72e6
ikonilovelive <140684321+mykonshshhdhdhdh@users.noreply.github.com>
2023-08-10 13:56:46 +0800

    Add files via upload

---
+++ an.js
@@ -0,0 +1,102 @@
+var y=1;
+
+
+function your(){
+document.getElementById("a").style.cssText="height:200px";
+document.getElementById("b").style.cssText="height:115px";
+document.getElementById("c").style.cssText="height:115px";
+document.getElementById("d").style.cssText="height:115px";
+document.getElementById("da").style.cssText="display:inline";
+document.getElementById("db").style.cssText="display:none";
+document.getElementById("dc").style.cssText="display:none";
+document.getElementById("dd").style.cssText="display:none";
+}
+function b(){
+document.getElementById("a").style.cssText="height:115px";
+document.getElementById("b").style.cssText="height:200px";
+document.getElementById("c").style.cssText="height:115px";
+document.getElementById("d").style.cssText="height:115px";
+document.getElementById("da").style.cssText="display:none";
+document.getElementById("db").style.cssText="display:inline";
+document.getElementById("dc").style.cssText="display:none";
+document.getElementById("dd").style.cssText="display:none";
+}
+function c(){
+document.getElementById("a").style.cssText="height:115px";
+document.getElementById("b").style.cssText="height:115px";
+document.getElementById("c").style.cssText="height:200px";
+document.getElementById("d").style.cssText="height:115px";
+document.getElementById("da").style.cssText="display:none";
+document.getElementById("db").style.cssText="display:none";
+document.getElementById("dc").style.cssText="display:inline";
+document.getElementById("dd").style.cssText="display:none";
+}
+function d(){
+document.getElementById("a").style.cssText="height:115px";
+document.getElementById("b").style.cssText="height:115px";
+document.getElementById("c").style.cssText="height:115px";
+document.getElementById("d").style.cssText="height:200px";
+document.getElementById("da").style.cssText="display:none";
+document.getElementById("db").style.cssText="display:none";
+document.getElementById("dc").style.cssText="display:none";
+document.getElementById("dd").style.cssText="display:inline";
+}
+function q(){
+ y=y+1;
+ if(y==6){
+ y=0;
+ }
+ if(y==1){
+ document.getElementById("z").style.cssText="display:inline";
+ document.getElementById("x").style.cssText="display:none";
+ document.getElementById("v").style.cssText="display:none";
+ document.getElementById("l").style.cssText="display:none";
+ document.getElementById("n").style.cssText="display:none";
+ document.getElementById("k").style.cssText="display:none";
+ }
+if(y==2){
+ document.getElementById("z").style.cssText="display:none";
+ document.getElementById("x").style.cssText="display:inline";
+ document.getElementById("v").style.cssText="display:none";
+ document.getElementById("l").style.cssText="display:none";
+ document.getElementById("n").style.cssText="display:none";
+ document.getElementById("k").style.cssText="display:none";
+ }
+ 
+ if(y==3){
+ document.getElementById("z").style.cssText="display:none";
+ document.getElementById("x").style.cssText="display:none";
+ document.getElementById("v").style.cssText="display:inline";
+ document.getElementById("l").style.cssText="display:none";
+ document.getElementById("n").style.cssText="display:none";
+ document.getElementById("k").style.cssText="display:none";
+ }
+ if(y==4){
+ document.getElementById("z").style.cssText="display:none";
+ document.getElementById("x").style.cssText="display:none";
+ document.getElementById("v").style.cssText="display:none";
+ document.getElementById("l").style.cssText="display:inline";
+ document.getElementById("n").style.cssText="display:none";
+ document.getElementById("k").style.cssText="display:none";
+ }
+ if(y==5){
+ document.getElementById("z").style.cssText="display:none";
+ document.getElementById("x").style.cssText="display:none";
+ document.getElementById("v").style.cssText="display:none";
+ document.getElementById("l").style.cssText="display:none";
+  document.getElementById("n").style.cssText="display:inline";
+ document.getElementById("k").style.cssText="display:none";
+ }
+ if(y==0){
+ document.getElementById("z").style.cssText="display:none";
+ document.getElementById("x").style.cssText="display:none";
+ document.getElementById("v").style.cssText="display:none";
+ document.getElementById("l").style.cssText="display:none";
+ document.getElementById("n").style.cssText="display:none";
+ document.getElementById("k").style.cssText="display:inline";
+ }
+ 
+ 
+
+ }
+
